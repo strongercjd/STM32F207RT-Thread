24e8eaaa999aef242e3e26ddc52ca29b6e5a0a5c
strongercjd <strongercjd@163.com>
2021-02-21 16:20:55 +0800

    添加信号量和临界区保护代码

---
+++ Examples/07-信号量/main.c
@@ -0,0 +1,256 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F2xx_StdPeriph_Template/main.c 
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    13-April-2012
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <rtthread.h>
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+void UART_Init(void);
+
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+
+
+#define THREAD_PRIORITY         25
+#define THREAD_TIMESLICE        5
+
+/* 指向信号量的指针 */
+static rt_sem_t dynamic_sem = RT_NULL;
+
+ALIGN(RT_ALIGN_SIZE)
+static char thread1_stack[1024];
+static struct rt_thread thread1;
+static void rt_thread1_entry(void *parameter)
+{
+    static rt_uint32_t count = 0;
+  
+    while(1)
+    {
+        if(count <= 100)
+        {
+            count++;           
+        }
+        else
+            return; 
+        
+        /* count每计数10次，就释放一次信号量 */
+         if(0 == (count % 10))
+        {
+            printf("%d t1 release a dynamic semaphore.\n" ,(int)count); 
+            rt_sem_release(dynamic_sem);		
+        }
+    }
+}
+
+ALIGN(RT_ALIGN_SIZE)
+static char thread2_stack[1024];
+static struct rt_thread thread2;
+static void rt_thread2_entry(void *parameter)
+{
+    static rt_err_t result;
+    static rt_uint8_t number = 0;
+    while(1)
+    {
+        /* 永久方式等待信号量，获取到信号量，则执行number自加的操作 */
+        result = rt_sem_take(dynamic_sem, RT_WAITING_FOREVER);
+        if (result != RT_EOK)
+        {        
+            printf("t2 take a dynamic semaphore, failed.\n");
+            rt_sem_delete(dynamic_sem);
+            return;
+        }
+        else
+        {      
+            number++;             
+            printf("t2 take a dynamic semaphore. number = %d\n" ,(int)number);                        
+        }
+    }   
+}
+
+/* 信号量示例的初始化 */
+int semaphore_sample()
+{
+    /* 创建一个动态信号量，初始值是0 */
+    dynamic_sem = rt_sem_create("dsem", 0, RT_IPC_FLAG_FIFO);
+    if (dynamic_sem == RT_NULL)
+    {
+        printf("create dynamic semaphore failed.\n");
+        return -1;
+    }
+    else
+    {
+        printf("create done. dynamic semaphore value = 0.\n");
+    }
+
+    rt_thread_init(&thread1,
+                   "thread1",
+                   rt_thread1_entry,
+                   RT_NULL,
+                   &thread1_stack[0],
+                   sizeof(thread1_stack), 
+                   THREAD_PRIORITY, THREAD_TIMESLICE);
+//    rt_thread_startup(&thread1);
+                   
+    rt_thread_init(&thread2,
+                   "thread2",
+                   rt_thread2_entry,
+                   RT_NULL,
+                   &thread2_stack[0],
+                   sizeof(thread2_stack), 
+                   THREAD_PRIORITY-1, THREAD_TIMESLICE);
+    rt_thread_startup(&thread2);
+		rt_thread_startup(&thread1);
+
+    return 0;
+}
+
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  GPIO_InitTypeDef  GPIO_Init_s;
+  
+  /* 使能GPIOE端口时钟 */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+  
+  /* 配置LED管脚 */
+  GPIO_Init_s.GPIO_Pin = GPIO_Pin_4;
+  GPIO_Init_s.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_Init_s.GPIO_OType = GPIO_OType_PP;
+  GPIO_Init_s.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init_s.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOE, &GPIO_Init_s);
+  
+  GPIO_SetBits(GPIOE,GPIO_Pin_4);//熄灭LED灯
+  
+  UART_Init();
+  
+  printf("\r\n======================================================================");
+  printf("\r\n=               (C) COPYRIGHT 2021                                   =");
+  printf("\r\n=                                                                    =");
+  printf("\r\n=                ST207 RTT thread                                    =");
+  printf("\r\n=                                                                    =");
+  printf("\r\n=                                           By Firefly               =");
+  printf("\r\n======================================================================");
+  printf("\r\n\r\n");
+  
+  semaphore_sample();
+  
+  while (1)
+  {
+    GPIO_SetBits(GPIOE,GPIO_Pin_4);  //熄灭LED灯
+    rt_thread_mdelay(500);
+    GPIO_ResetBits(GPIOE,GPIO_Pin_4);//点亮LED灯
+    rt_thread_mdelay(500);//延时500ms
+  }
+}
+void UART_Init(void)
+{
+  USART_InitTypeDef USART_InitStructure;
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* Enable GPIO clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  /* Enable UART1 clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+  /* Connect PXx to USARTx_Tx*/
+  GPIO_PinAFConfig(GPIOA, 9, GPIO_AF_USART1);
+  
+  /* Connect PXx to USARTx_Rx*/
+  GPIO_PinAFConfig(GPIOA, 10, GPIO_AF_USART1);
+  
+  /* Configure USART Tx as alternate function  */
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  /* Configure USART Rx as alternate function  */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  
+  /* USART configuration */
+  USART_Init(USART1, &USART_InitStructure);
+  
+  /* Enable USART */
+  USART_Cmd(USART1, ENABLE);
+}
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART */
+  USART_SendData(USART1, (uint8_t) ch);
+
+  /* Loop until the end of transmission */
+  while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
+  {}
+
+  return ch;
+}
+
+
+
+
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
