f6dcd95ce0008a939be2cf6fb7fba8b92ee95a07
strongercjd <strongercjd@163.com>
2021-02-21 13:27:54 +0800

    添加空闲线程和钩子函数代码

---
+++ Examples/05-系统调度钩子函数/main.c
@@ -0,0 +1,218 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F2xx_StdPeriph_Template/main.c 
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    13-April-2012
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <rtthread.h>
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+void UART_Init(void);
+
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+
+
+#define THREAD_STACK_SIZE	1024
+#define THREAD_PRIORITY	  20
+#define THREAD_TIMESLICE  10
+
+/* 线程入口 */
+static void thread_entry(void* parameter)
+{
+  rt_uint32_t value;
+  rt_uint32_t count = 0;
+  
+  value = (rt_uint32_t)parameter;
+  while (1)
+  {       
+		if(0 == (count % 10))
+		{
+			printf("thread %d is running ,thread %d count = %d\n", (int)value , (int)value , (int)count);      
+			if(count > 100)
+				return;  
+		}			
+		count++;
+			
+  }  
+}
+
+static void hook_of_scheduler(struct rt_thread* from, struct rt_thread* to)
+{
+    printf("\r\n\r\nfrom: %s -->  to: %s \n", from->name , to->name);
+}
+
+int timeslice_sample(void)
+{
+  rt_thread_t tid;
+	
+	/* 设置调度器钩子 */
+	rt_scheduler_sethook(hook_of_scheduler);
+  
+  /* 创建线程1 */
+  tid = rt_thread_create("thread1", 
+                         thread_entry, (void*)1, 
+                         THREAD_STACK_SIZE, 
+                         THREAD_PRIORITY, THREAD_TIMESLICE); 
+  if (tid != RT_NULL) 
+    rt_thread_startup(tid);
+  
+  
+  /* 创建线程2 */
+  tid = rt_thread_create("thread2", 
+                         thread_entry, (void*)2,
+                         THREAD_STACK_SIZE, 
+                         THREAD_PRIORITY, THREAD_TIMESLICE-5);
+  if (tid != RT_NULL) 
+    rt_thread_startup(tid);
+  return 0;
+}
+
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  GPIO_InitTypeDef  GPIO_Init_s;
+  
+  /* 使能GPIOE端口时钟 */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+  
+  /* 配置LED管脚 */
+  GPIO_Init_s.GPIO_Pin = GPIO_Pin_4;
+  GPIO_Init_s.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_Init_s.GPIO_OType = GPIO_OType_PP;
+  GPIO_Init_s.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init_s.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOE, &GPIO_Init_s);
+  
+  GPIO_SetBits(GPIOE,GPIO_Pin_4);//熄灭LED灯
+  
+  UART_Init();
+  
+  printf("\r\n======================================================================");
+  printf("\r\n=               (C) COPYRIGHT 2021                                   =");
+  printf("\r\n=                                                                    =");
+  printf("\r\n=                ST207 RTT thread                                    =");
+  printf("\r\n=                                                                    =");
+  printf("\r\n=                                           By Firefly               =");
+  printf("\r\n======================================================================");
+  printf("\r\n\r\n");
+  
+  timeslice_sample();
+  
+  while (1)
+  {
+    GPIO_SetBits(GPIOE,GPIO_Pin_4);  //熄灭LED灯
+    rt_thread_mdelay(500);
+    GPIO_ResetBits(GPIOE,GPIO_Pin_4);//点亮LED灯
+    rt_thread_mdelay(500);//延时500ms
+  }
+}
+void UART_Init(void)
+{
+  USART_InitTypeDef USART_InitStructure;
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* Enable GPIO clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  /* Enable UART1 clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+  /* Connect PXx to USARTx_Tx*/
+  GPIO_PinAFConfig(GPIOA, 9, GPIO_AF_USART1);
+  
+  /* Connect PXx to USARTx_Rx*/
+  GPIO_PinAFConfig(GPIOA, 10, GPIO_AF_USART1);
+  
+  /* Configure USART Tx as alternate function  */
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  /* Configure USART Rx as alternate function  */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  
+  /* USART configuration */
+  USART_Init(USART1, &USART_InitStructure);
+  
+  /* Enable USART */
+  USART_Cmd(USART1, ENABLE);
+}
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART */
+  USART_SendData(USART1, (uint8_t) ch);
+
+  /* Loop until the end of transmission */
+  while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
+  {}
+
+  return ch;
+}
+
+
+
+
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
